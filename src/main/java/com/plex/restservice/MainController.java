5a92f4f3efa8b7aa48138db1fe61009cc515fc38
Leon <Leonvandervleuten@gmail.com>
2021-11-20 14:02:12 +0100

    Post request werkend

--- src/main/java/com/plex/restservice/MainController.java
+++ src/main/java/com/plex/restservice/MainController.java
@@ -1,6 +1,13 @@
 package com.plex.restservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.plex.restservice.Dto.NameAndIdList;
 import com.plex.restservice.challenge.Challenge;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,20 +26,30 @@ public class MainController {
 
     @CrossOrigin
     @GetMapping("/challenges")
-    public List<Challenge> getChallenges(){
+    public List<Challenge> getChallenges() {
         return mainService.getChallenges();
     }
 
 
     @CrossOrigin
     @GetMapping("/challenges/{id}")
-    public Challenge getChallengesById(@PathVariable("id") String id){
+    public Challenge getChallengesById(@PathVariable("id") String id) {
         return mainService.getChallengeById(Long.parseLong(id));
     }
 
     @CrossOrigin
     @GetMapping("/challengelist/{id}")
-    public List<Challenge> getChallengeList(@PathVariable("id") String id){
+    public List<Challenge> getChallengeList(@PathVariable("id") String id) {
         return mainService.getChallengeList(Long.parseLong(id));
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/projectlist")
+    public String getData(@RequestBody NameAndIdList nameAndIdList) throws JSONException, JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(nameAndIdList);
+        JSONObject object = new JSONObject(json);
+        return mainService.createProjectList(object).toString();
+
     }
 }
