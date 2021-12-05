5a0825421b4a7b249a0ae3f9bef48e377c18c87c
Yuri Vugt <184522@student.fontys.nl>
2021-12-05 12:14:58 +0100

    create modules for separating apis and data

---
+++ dexapi/src/main/java/com/plex/dexapi/domain/Like.java
@@ -0,0 +1,11 @@
+package com.plex.dexapi.domain;
+
+import java.util.Date;
+
+public class Like {
+  public int id;
+  public String path;
+  public Date uploadDateTime;
+  public String name;
+  public int uploaderUserId;
+}
--- src/main/java/com/plex/restservice/domain/LinkedInstitution.java
+++
@@ -1,8 +0,0 @@
-package com.plex.restservice.domain;
-
-public class LinkedInstitution {
-  public int institutionId;
-  public String instititutionName;
-  public int projectId;
-  public String projectName;
-}
