86ab6542aad9592c249de7b60a4d5076d76d885c
Yuri Vugt <184522@student.fontys.nl>
2021-12-07 21:58:39 +0100

    add repo and entity annotation to starter

--- plexapi/src/main/java/com/plex/plexapi/RestServiceApplication.java
+++ plexapi/src/main/java/com/plex/plexapi/RestServiceApplication.java
@@ -2,8 +2,12 @@ package com.plex.plexapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.plex")
+@EnableJpaRepositories("com.plex.data")
+@EntityScan("com.plex.data")
 public class RestServiceApplication {
 
   public static void main(String[] args) {
